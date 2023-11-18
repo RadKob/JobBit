e2001aa574f91a0ea0beef36e15e45a56917c277
rkobus <radek109@o2.pl>
2023-11-18 15:03:44 +0100

    Dodaj pliki projektów.

---
+++ JobBit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobBit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
