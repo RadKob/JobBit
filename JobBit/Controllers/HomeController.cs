20adbae924e487683afa760c97e84dda0e49b252
rkobus <radek109@o2.pl>
2023-11-18 18:43:25 +0100

    Dodanie strony startowej

---
+++ JobBit/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobBit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
