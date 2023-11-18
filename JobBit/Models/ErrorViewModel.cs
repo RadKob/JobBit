1f3e6669f90a1592f30e7b8a8c3946f0241bb3e8
rkobus <radek109@o2.pl>
2023-11-18 16:07:49 +0100

    Reset projektu :)

--- JobBit/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JobBit.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
